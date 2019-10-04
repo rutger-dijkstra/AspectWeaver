bd1aa20fb96f94ac281d1fe3650ed01d439ca9f3
rutger dijkstra <rutger.dijkstra@thelearningnetwork.nl>
2019-10-04 16:42:44 +0200

    Reimplemented Retrying with a direct implementation of a retrying method invoker instead of the weaving invoker (commit 2nd half)

---
+++ src/AspectRetry/RetryInvoker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using AspectWeaver;
+
+namespace AspectRetry {
+
+  class RetryInvoker: IMethodInvoker {
+
+    readonly IMethodInvoker _inner;
+    readonly IRetryStrategy _retryStrategy;
+
+    public RetryInvoker(IMethodInvoker inner, IRetryStrategy retryStrategy) {
+      _inner = inner;
+      _retryStrategy = retryStrategy;
+    }
+
+    public void InvokeAction(MethodInfo targetMethod, object[] args) {
+      var delays = _retryStrategy.Delays.GetEnumerator();
+      for(; ; ) {
+        try {
+          _inner.InvokeAction(targetMethod, args);
+          return;
+        } catch( Exception e ) when( WillRetry(e, delays) ) {
+          Thread.Sleep(delays.Current);
+        }
+      }
+    }
+
+    public S InvokeFunc<S>(MethodInfo targetMethod, object[] args) {
+      var delays = _retryStrategy.Delays.GetEnumerator();
+      for(; ; ) {
+        try {
+          return _inner.InvokeFunc<S>(targetMethod, args);
+        } catch( Exception e ) when( WillRetry(e, delays) ) {
+          Thread.Sleep(delays.Current);
+        }
+      }
+    }
+
+    public async Task InvokeActionAsync(MethodInfo targetMethod, object[] args) {
+      var delays = _retryStrategy.Delays.GetEnumerator();
+      for(; ; ) {
+        try {
+          await _inner.InvokeActionAsync(targetMethod, args);
+          return;
+        } catch( Exception e ) when( WillRetry(e, delays) ) {
+          await Task.Delay(delays.Current);
+        }
+      }
+    }
+
+    public async Task<S> InvokeFuncAsync<S>(MethodInfo targetMethod, object[] args) {
+      var delays = _retryStrategy.Delays.GetEnumerator();
+      for(; ; ) {
+        try {
+          return await _inner.InvokeFuncAsync<S>(targetMethod, args);
+        } catch( Exception e ) when( WillRetry(e, delays) ) {
+          await Task.Delay(delays.Current);
+        }
+      }
+    }
+
+    bool WillRetry(Exception e, IEnumerator<TimeSpan> delays) =>
+      delays.MoveNext() && _retryStrategy.ShouldRetry(e);
+  }
+}
+
